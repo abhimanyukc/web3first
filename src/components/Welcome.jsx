19e3f25f9ecb6e8a48f286f02a96ec4eadd1255d
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-05 19:19:02 +0545

    using componets setup and navbar code

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+//first part of our appln
+const Welcome = () => {
+    return (
+      <div>Welcome</div>
+    )
+  }
+  
+  export default Welcome
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+//to allow our application to import all of these components in one line in app.jsx
+//export all component from single file
+export {default  as Loader} from './Loader';
+export {default  as Footer} from './Footer';
+export {default  as Navbar} from './Navbar';
+export {default  as Services} from './Services';
+export {default  as Transactions} from './Transactions';
+export {default  as Welcome} from './Welcome';
+
+
+
+
+
+
+
+
